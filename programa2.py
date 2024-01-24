f6c02d2fb6bbe551ae92911860d407310a9f9b58
jcastil1o <157521655+jcastil1o@users.noreply.github.com>
2024-01-24 08:56:58 -0600

    Add files via upload

---
+++ programa2.py
@@ -0,0 +1,11 @@
+##Sentencia If
+##Uso de modulo de residuo
+
+numero = int (input("Ingrese un numero: "))
+##condicional
+if numero % 2 == 0:
+    ##== es una comparacion
+    print("El numero es par")
+
+else:
+    print("El numero es impar")
