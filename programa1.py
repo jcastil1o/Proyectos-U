e8d36132393ff5de0ca61028c7c028ed759a7038
jcastil1o <jcastillom37@miumg.edu.gt>
2024-01-24 08:55:12 -0600

    mensaje

---
+++ programa1.py
@@ -0,0 +1,26 @@
+edad = 20
+nombre = "Jonathan"
+caracter = "$"
+boleano = True
+flotante = 5.03
+
+a = 3
+b = 4
+
+suma = a + b
+restar = a-b
+multiplicacion = a*b
+division = a/b
+potencia = a**b
+
+print("Resultados de operaciones aritméticas")
+print("Suma: ", suma, "Resta: ", restar, "Multiplicacion: ", multiplicacion, "División: ", division, "Potencia: ", potencia)
+
+##Comentario
+
+##Concatenacion de dos tipos de variables
+sumarTiposVariables = edad + nombre
+print("Suma de variables: ", sumarTiposVariables)
+
+"""Comentario
+    de Bloque de texto """
