6ec698483fa6f045f2e5abc9989b48ab6dffd2c4
jcastil1o <jcastillom37@miumg.edu.gt>
2024-01-25 10:43:40 -0600

    Correccion de codigo

--- Variables_Tarea1.py
+++ Variables_Tarea1.py
@@ -3,20 +3,21 @@ print("     Programa para conocer el tipo de variable")
 print("Ingrese algo a la consola")
 dato = input("Dato: ")
 
-def determinar_tipo_variable(valor) :
-## Determina el tipo de la variable utilizando la función type()
-tipo = type(dato)._name_
-return tipo
+def determinar_tipo_variable(valor):
+    # Determina el tipo de la variable utilizando la función type()
+    tipo = type(valor)._name_
+    return tipo
 
-## Solicita al usuario ingresar una variable
-entrada_usuario = input("lngrese una variable: ")
+# Solicita al usuario ingresar una variable
+entrada_usuario = input("Ingrese una variable: ")
 
-## intenta convertir la entrada del usuario a un numero si es posible
+# Intenta convertir la entrada del usuario a un número si es posible
 try:
-    entrada_usuario = eval(entrada_usuario) 
-except: pass
+    entrada_usuario = eval(entrada_usuario)
+except:
+    pass
 
-##Determenia y muestra el tipo de la variable
+# Determina y muestra el tipo de la variable
 tipo_variable = determinar_tipo_variable(entrada_usuario)
-print(f"El tipo de la variable es: {determinar_tipo_variable}")
+print(f"El tipo de la variable es: {tipo_variable}")
 ##Jonathan Castillo 0901-22-8408
