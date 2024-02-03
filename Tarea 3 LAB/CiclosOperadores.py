d966ed1b2aa962c584c5cadbd519e271497c1f00
jcastil1o <jcastillom37@miumg.edu.gt>
2024-02-03 14:19:34 -0600

    Completado 3er ejercicio

---
+++ Tarea 3 LAB/CiclosOperadores.py
@@ -0,0 +1,8 @@
+##Programa para imprimir los primeros 10 numeros en un ciclo 
+mensaje = "Programa de impresion de los primeros 10 numeros"
+print(mensaje.center(100, ":"))
+for contador in range (0, 10):
+    print (contador, ' ', end= ' ')
+print()
+print("Fin del programa...")
+##Jonathan Castillo0901-22-8408
