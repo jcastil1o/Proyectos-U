2d7b7d15a86e68b605cbf517dd7ef77a5c765ed4
jcastil1o <jcastillom37@miumg.edu.gt>
2024-02-10 15:51:11 -0600

    Correjido y completo

--- Tarea 4/5_MediaCola.py
+++ Tarea 4/5_MediaCola.py
@@ -1,52 +1,36 @@
 #Programa de implementar una cola y reversion del orden de la mitad de la cosa en una lista
 from collections import deque
-    #Importar libreria de cola
-import time
-    #Importar libreria de tiempo
-
-class Cola:
-    def __init__(self, lista):
-        self.cola = deque(lista)   
-    def __str__(self):
-        return str(self.cola)
-    def enqueue(self, elemento):
-        self.cola.append(elemento)
-    def dequeue (self):
-        return self.cola.popleft() 
-
-def cola(cola):
-    #Definir funcion de cola
-    queue = deque (cola)
-    while queue:
-        print ("Orden de cola: ", queue.popleft(), "\n")
-        #Impresion de cola
-            #metodo de mostrar el primer elemento de la lista en cola
-        time.sleep(1)
-
-def colarevertir(cola):
+#Importar libreria de cola
+def revertir_cola(cola):
+    #definir funcion de cola
     pila = []
-    longitud_cola = len(cola)
-    mitad_longitud = longitud_cola//2
+    longitud = len(cola)
+    #Obtener tamaño de cola
+    mitad_cola = longitud // 2
+    #Encontrar la mitad de la cola
 
-    for _ in range (mitad_longitud):
-        elemento = cola.dequeue()
-        pila.append(elemento)
+    for _ in range(mitad_cola):
+        pila.append(cola.popleft())
+        #encontrar el primer dato en cola de la mitad
 
-    for _ in range (longitud_cola):
-        elemento = pila.pop()
-        cola.enqueue(elemento)
-        
-        return cola
+    for _ in range(mitad_cola):
+        pila.append(cola.pop())
+        #añadir elemento a la mitad de cola
+    
+    if longitud % 2 !=0:
+        cola.append(pila.pop())
+        #Verificar si la mitad de la cola es par o impar para dejar un espacio vacio
 
-print("Ingrese los elementos de la lista: (SEPARADOS POR ESPACIOS)".center(50, " "))
-lista = input()
-#Ingreso de elementos para la lista
-lista = [float (x) for x in lista.split()]
-    #Declaracion de lista
-cola(lista)
-cola_invertida = colarevertir(deque(lista))
-print("Orden de cola invertida: ")
-while cola_invertida:
-    print(cola_invertida.popleft(), "\n")
-    time.sleep(1)
-""" Inserte aqui para declarar el metodo de ordenar la cola en mitad"""
+    while pila:
+        cola.appendleft(pila.pop())
+        #ciclo para mostrar el primer dato en cola
+    return cola
+
+lista_cola = deque(input("Ingrese los elementos de la lista: "))
+#pedir los datos de la lista para volverlo cola
+print("cola original: ", lista_cola)
+#mostrar lista original
+lista_cola = revertir_cola(lista_cola)
+#poner en funcionamiento la mitad de reversion de la cola
+print("Mitad revertida: ", lista_cola)
+#mostrar la nueva cola invertida
