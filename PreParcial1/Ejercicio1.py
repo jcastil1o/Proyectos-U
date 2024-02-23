28d4d3e03ae0caaf37292e356e1b58beec08e347
jcastil1o <jcastillom37@miumg.edu.gt>
2024-02-23 11:30:59 -0600

    Completado

--- PreParcial1/Ejercicio1.py
+++ PreParcial1/Ejercicio1.py
@@ -5,4 +5,20 @@ class TreeNode(object):
         self.right = None
 
 def sorted_array_to_bst(nums):
-    
+    if not nums:
+        return None
+    mid_val = len(nums)//2
+    node = TreeNode(nums[mid_val])
+    node.left = sorted_array_to_bst(nums[:mid_val])
+    node.right = sorted_array_to_bst(nums[mid_val+1:])
+    return node
+
+def preOrder(node):
+    if not node:
+        return
+    print(node.val)
+    preOrder(node.left)
+    preOrder(node.right)
+
+result = sorted_array_to_bst([1, 2, 3, 4, 5, 6, 7])
+preOrder(result)
