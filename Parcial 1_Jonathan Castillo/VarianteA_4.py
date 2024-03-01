f28002ff8f320fe55960a534b9718cf2241dca26
jcastil1o <jcastillom37@miumg.edu.gt>
2024-03-01 11:50:59 -0600

    casi completo este programa

---
+++ Parcial 1_Jonathan Castillo/VarianteA_4.py
@@ -0,0 +1,11 @@
+"""Parcial 1
+    Jonathan Camilo Castillo Mendez 0901 - 22 - 8408 """
+"""Parcial 1
+    Jonathan Camilo Castillo Mendez 0901 - 22 - 8408 """
+
+"""Crea un diccionario en Python que represente un libro, con claves como "título", "autor" y "año de 
+publicación". Luego, escribe un código que agregue un nuevo campo al diccionario, como "género", y 
+lo imprima."""
+
+def libro(self):
+    
