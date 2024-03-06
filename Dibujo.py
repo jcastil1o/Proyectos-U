8db89dac49068dc1bd2ac9b4c66b5457166ab5bd
jcastil1o <jcastillom37@miumg.edu.gt>
2024-03-06 17:09:30 -0600

    Jose

---
+++ Dibujo.py
@@ -0,0 +1,337 @@
+import turtle
+#librerias de tiempo y dibujo
+
+#Color y declaracion de pantalla de fondo
+fondo = turtle.Screen()
+fondo.bgcolor("black")
+
+#declarar dibujo
+cuadro1 = turtle.Turtle()
+#velocidad de trazado
+cuadro1.speed(10)
+#Relleno de dibujo
+cuadro1.color("orange")
+cuadro1.begin_fill()
+#lapiz arriba
+cuadro1.penup()
+#posicionamiento del dibujo
+cuadro1.goto(-325, 150)
+#lapiz abajo
+cuadro1.pendown()
+#ciclo para el trazado
+for x in range(2):
+    cuadro1.color("orange")
+    cuadro1.forward(125)
+    cuadro1.right(90)
+    cuadro1.forward(25)
+    cuadro1.right(90)
+cuadro1.end_fill()
+    #SEGUNGO CUADRO
+#declarar dibujo
+cuadro2 = turtle.Turtle()
+#velocidad de trazado
+cuadro2.speed(10)
+#Relleno de dibujo
+cuadro2.color("orange")
+cuadro2.begin_fill()
+#lapiz arriba
+cuadro2.penup()
+#posicionamiento del dibujo
+cuadro2.goto(-325, 25)
+#lapiz abajo
+cuadro2.pendown()
+#ciclo para el trazado
+for x in range(2):
+    cuadro2.color("orange")
+    cuadro2.forward(125)
+    cuadro2.right(90)
+    cuadro2.forward(25)
+    cuadro2.right(90)
+cuadro2.end_fill()
+#fin de salida del dibujo
+    #CUADRO 3
+#declarar dibujo
+cuadro3 = turtle.Turtle()
+#velocidad de trazado
+cuadro3.speed(10)
+#Relleno de dibujo
+cuadro3.color("orange")
+cuadro3.begin_fill()
+#lapiz arriba
+cuadro3.penup()
+#posicionamiento del dibujo
+cuadro3.goto(-350, 125)
+#lapiz abajo
+cuadro3.pendown()
+#ciclo para el trazado
+for x in range(2):
+    cuadro3.color("orange")
+    cuadro3.forward(25)
+    cuadro3.right(90)
+    cuadro3.forward(35)
+    cuadro3.right(90)
+cuadro3.end_fill()
+    #CUADRO 4
+#declarar dibujo
+cuadro4 = turtle.Turtle()
+#velocidad de trazado
+cuadro4.speed(10)
+#Relleno de dibujo
+cuadro4.color("orange")
+cuadro4.begin_fill()
+#lapiz arriba
+cuadro4.penup()
+#posicionamiento del dibujo
+cuadro4.goto(-365, 90)
+#lapiz abajo
+cuadro4.pendown()
+#ciclo para el trazado
+for x in range(2):
+    cuadro4.color("orange")
+    cuadro4.forward(25)
+    cuadro4.right(90)
+    cuadro4.forward(35)
+    cuadro4.right(90)
+cuadro4.end_fill()
+    #CUADRO 5
+#declarar dibujo
+cuadro5 = turtle.Turtle()
+#velocidad de trazado
+cuadro5.speed(10)
+#Relleno de dibujo
+cuadro5.color("orange")
+cuadro5.begin_fill()
+#lapiz arriba
+cuadro5.penup()
+#posicionamiento del dibujo
+cuadro5.goto(-345, 55)
+#lapiz abajo
+cuadro5.pendown()
+#ciclo para el trazado
+for x in range(2):
+    cuadro5.color("orange")
+    cuadro5.forward(25)
+    cuadro5.right(90)
+    cuadro5.forward(35)
+    cuadro5.right(90)
+cuadro5.end_fill()
+    #CUADRO 6
+#declarar dibujo
+cuadro6 = turtle.Turtle()
+#velocidad de trazado
+cuadro6.speed(10)
+#Relleno de dibujo
+cuadro6.color("orange")
+cuadro6.begin_fill()
+#lapiz arriba
+cuadro6.penup()
+#posicionamiento del dibujo
+cuadro6.goto(-300, 175)
+#lapiz abajo
+cuadro6.pendown()
+#ciclo para el trazado
+for x in range(2):
+    cuadro6.color("orange")
+    cuadro6.forward(25)
+    cuadro6.right(90)
+    cuadro6.forward(25)
+    cuadro6.right(90)
+cuadro6.end_fill()
+ #CUADRO 7
+#declarar dibujo
+cuadro7 = turtle.Turtle()
+#velocidad de trazado
+cuadro7.speed(10)
+#Relleno de dibujo
+cuadro7.color("orange")
+cuadro7.begin_fill()
+#lapiz arriba
+cuadro7.penup()
+#posicionamiento del dibujo
+cuadro7.goto(-245, 175)
+#lapiz abajo
+cuadro7.pendown()
+#ciclo para el trazado
+for x in range(2):
+    cuadro7.color("orange")
+    cuadro7.forward(25)
+    cuadro7.right(90)
+    cuadro7.forward(25)
+    cuadro7.right(90)
+cuadro7.end_fill()
+"""
+    DIBUJOS DEL LADO DERECHO
+"""
+    #CUADRO 8
+#declarar dibujo
+cuadro8 = turtle.Turtle()
+#velocidad de trazado
+cuadro8.speed(10)
+#Relleno de dibujo
+cuadro8.color("orange")
+cuadro8.begin_fill()
+#lapiz arriba
+cuadro8.penup()
+#posicionamiento del dibujo
+cuadro8.goto(-200, 125)
+#lapiz abajo
+cuadro8.pendown()
+#ciclo para el trazado
+for x in range(2):
+    cuadro8.color("orange")
+    cuadro8.forward(25)
+    cuadro8.right(90)
+    cuadro8.forward(35)
+    cuadro8.right(90)
+cuadro8.end_fill()
+    #CUADRO 9
+#declarar dibujo
+cuadro9 = turtle.Turtle()
+#velocidad de trazado
+cuadro9.speed(10)
+#Relleno de dibujo
+cuadro9.color("orange")
+cuadro9.begin_fill()
+#lapiz arriba
+cuadro9.penup()
+#posicionamiento del dibujo
+cuadro9.goto(-185, 90)
+#lapiz abajo
+cuadro9.pendown()
+#ciclo para el trazado
+for x in range(2):
+    cuadro9.color("orange")
+    cuadro9.forward(25)
+    cuadro9.right(90)
+    cuadro9.forward(35)
+    cuadro9.right(90)
+cuadro9.end_fill()
+    #CUADRO 10
+#declarar dibujo
+cuadro10 = turtle.Turtle()
+#velocidad de trazado
+cuadro10.speed(10)
+#Relleno de dibujo
+cuadro10.color("orange")
+cuadro10.begin_fill()
+#lapiz arriba
+cuadro10.penup()
+#posicionamiento del dibujo
+cuadro10.goto(-200, 55)
+#lapiz abajo
+cuadro10.pendown()
+#ciclo para el trazado
+for x in range(2):
+    cuadro10.color("orange")
+    cuadro10.forward(25)
+    cuadro10.right(90)
+    cuadro10.forward(35)
+    cuadro10.right(90)
+cuadro10.end_fill()
+
+"""
+    DIBUJO DEL ROSTRO
+"""
+ #CUADRO 11
+#declarar dibujo
+cuadro11 = turtle.Turtle()
+#velocidad de trazado
+cuadro11.speed(10)
+#Relleno de dibujo
+cuadro11.color("orange")
+cuadro11.begin_fill()
+#lapiz arriba
+cuadro11.penup()
+#posicionamiento del dibujo
+cuadro11.goto(-290, 85)
+#lapiz abajo
+cuadro11.pendown()
+#ciclo para el trazado
+for x in range(2):
+    cuadro11.color("orange")
+    cuadro11.forward(10)
+    cuadro11.right(90)
+    cuadro11.forward(10)
+    cuadro11.right(90)
+cuadro11.end_fill()
+
+ #CUADRO 12
+#declarar dibujo
+cuadro12 = turtle.Turtle()
+#velocidad de trazado
+cuadro12.speed(10)
+#Relleno de dibujo
+cuadro12.color("orange")
+cuadro12.begin_fill()
+#lapiz arriba
+cuadro12.penup()
+#posicionamiento del dibujo
+cuadro12.goto(-250, 85)
+#lapiz abajo
+cuadro12.pendown()
+#ciclo para el trazado
+for x in range(2):
+    cuadro12.color("orange")
+    cuadro12.forward(10)
+    cuadro12.right(90)
+    cuadro12.forward(10)
+    cuadro12.right(90)
+cuadro12.end_fill()
+
+ #CUADRO 13
+#declarar dibujo
+cuadro13 = turtle.Turtle()
+#velocidad de trazado
+cuadro13.speed(10)
+#Relleno de dibujo
+cuadro13.color("orange")
+cuadro13.begin_fill()
+#lapiz arriba
+cuadro13.penup()
+#posicionamiento del dibujo
+cuadro13.goto(-270, 50)
+#lapiz abajo
+cuadro13.pendown()
+#ciclo para el trazado
+for x in range(2):
+    cuadro13.color("orange")
+    cuadro13.forward(20)
+    cuadro13.right(90)
+    cuadro13.forward(5)
+    cuadro13.right(90)
+cuadro13.end_fill()
+
+""" 
+    LINEA QUE SEPARA EL DIBUJO Y EL MENSAJE
+"""
+#Linea 1
+#declarar dibujo
+linea1 = turtle.Turtle()
+#velocidad de trazado
+linea1.speed(10)
+#Relleno de dibujo
+linea1.color("gray")
+linea1.begin_fill()
+#lapiz arriba
+linea1.penup()
+#posicionamiento del dibujo
+linea1.goto(5, 450)
+#lapiz abajo
+linea1.pendown()
+#ciclo para el trazado
+for x in range(2):
+    linea1.color("gray")
+    linea1.forward(5)
+    linea1.right(90)
+    linea1.forward(800)
+    linea1.right(90)
+linea1.end_fill()
+
+texto1 = turtle.Turtle()
+texto1.setpos(50, 100)
+mensaje1 = "De: XXX"
+texto1.write(mensaje1, font = ("Arial", 14, "bold"))
+
+
+turtle.exitonclick()
+#fin del programa
