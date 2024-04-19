6dcac44bad0f51e64ce4f7386052f36b20a588fe
jcastil1o <jcastillom37@miumg.edu.gt>
2024-04-18 18:18:26 -0600

    Arreglos varios

--- Arduino/Lab7.py
+++ Arduino/Lab7.py
@@ -53,19 +53,19 @@ def PreCirculo():
     canvas.delete("circle")
     interfaz.after(1, circulo1)
     interfaz.after(100, circulo2)
-    interfaz.after(12000, circulo3)
+    interfaz.after(10000, circulo3)
     
 def InCirculo():
     canvas.delete("circle")
     interfaz.after(1, circulo2)
     interfaz.after(100, circulo1)
-    interfaz.after(12000, circulo3)
+    interfaz.after(10000, circulo3)
     
 def PostCirculo():
     canvas.delete("circle")
     interfaz.after(1, circulo2)
     interfaz.after(100, circulo3)
-    interfaz.after(12000, circulo1)
+    interfaz.after(10000, circulo1)
     
 def ApagarCirculo():
     canvas.delete("circle")
@@ -78,6 +78,7 @@ def actualizar_barra():
             valor = int(datos)  # Intenta la conversión directamente (opcional)
             valor_normal = int((valor / 1023) * 100)
             canvas.coords(bar, 5, 105 - valor_normal, 35, 105)
+            print(datos, "\n")
             interfaz.update_idletasks()
             interfaz.update()
        except ValueError:  # Maneja errores de conversión
@@ -105,7 +106,8 @@ Apagar_boton.place(x=400, y=150)
 canvas = tk.Canvas(interfaz, width=400, height=300)
 canvas.pack()
 bar = canvas.create_rectangle(0, 105, 35, 105, fill="pale green")
-bar_text = canvas.create_text(200, 250, text= "", font = ("Resist Mono Variable Italic", 12, "bold"), anchor=tk.W, fill='black', angle=90)
+bar_text = canvas.create_text(200, 250, text="", font = ("Resist Mono Variable Italic", 12, "bold"), anchor=tk.W, fill='black', angle=90)
+
 
 canvas.configure(background="papaya whip")
 canvas.place(x=105, y=200)
